b9fdff840f4bb51eb2f471d9181c146f5b430d21
Laughing <Laughing_thg@163.com>
2021-05-12 23:42:53 +0800

    创建头部底部插件

---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main">
+        <div>{{msg}}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'login',
+    data(){
+        return{
+            msg: '底部'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="main">
+        <div>{{msg}}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'login',
+    data(){
+        return{
+            msg: '头部'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
